004690023fd567074c0feb6b895ab9390dfb49dc
Anthony Ryan <aryan@masseyservices.com>
2020-03-05 15:49:43 -0500

    Initial Commit

---
+++ static/js/app.js
@@ -0,0 +1,34 @@
+var request = require('request'); // "Request" library
+
+var client_id = 'bb7c475418484e7784d9cd25b5f9f52c'; // Your client id
+var client_secret = 'b0da0baeeab1499884912aea11f4ca58'; // Your secret
+
+// your application requests authorization
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    var options = {
+      url: 'https://api.spotify.com/v1/users/tonyryanworldwide',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
+    };
+    request.get(options, function(error, response, body) {
+      console.log(body);
+    });
+  }
+});
